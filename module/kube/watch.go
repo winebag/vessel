8cdf30dc5693e02ac0ade2216b1c9f57e72901c9
linzhinan(zenlin) <linzhinan@huawei.com>
2016-04-13 01:32:09 +0800

    update watch for check pod status with timeout
    Signed-off-by: linzhinan(zenlin) <linzhinan@huawei.com>

--- module/kube/watch.go
+++ module/kube/watch.go
@@ -1,20 +1,73 @@
 package kube
 
 import (
+	"errors"
 	"fmt"
+	"time"
 
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/labels"
+	"k8s.io/kubernetes/pkg/watch"
 )
 
-const (
-	RUNNING = "running"
-	ERROR   = "error"
-	DELETED = "deleted"
-	PENDING = "pending"
+/*
+Example:
+package main
+
+import (
+	"fmt"
+	"github.com/zenlinTechnofreak/vessel/module/kube"
 )
 
-func WatchPod(podName string, podNamespace string, c chan string) {
+func main() {
+	kube.New("127.0.0.1:8080")
+	b, err := kube.CheckPodStatus("", "app", "nginx", 30, "ADDED")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(b)
+}
+
+*/
+
+// CheckDelPod return status of the operation(specified by checkOp) of the pod, OK, TIMEOUT.
+func CheckPodStatus(podNamespace string, labelKey string, labelValue string, timeout int, checkOp string) (string, error) {
+	if checkOp != string(watch.Deleted) && checkOp != string(watch.Added) {
+		fmt.Errorf("Params checkOp err, checkOp: %v", checkOp)
+	}
+
+	//opts := api.ListOptions{FieldSelector: fields.Set{"kind": "pod"}.AsSelector()}
+	opts := api.ListOptions{LabelSelector: labels.Set{labelKey: labelValue}.AsSelector()}
+
+	w, err := CLIENT.Pods(podNamespace).Watch(opts)
+	if err != nil {
+		fmt.Errorf("Get watch interface err")
+	}
+
+	t := time.NewTimer(time.Second * time.Duration(timeout))
+
+	for {
+		select {
+		case event, ok := <-w.ResultChan():
+			//fmt.Println(event.Type)
+			if !ok {
+				fmt.Errorf("Watch err\n")
+				return "", errors.New("error occours from watch chanle")
+			}
+			//fmt.Println(event.Type)
+
+			if string(event.Type) == checkOp {
+				return "OK", nil
+			}
+
+		case <-t.C:
+			return "TIMEOUT", nil
+		}
+	}
+}
+
+/*func WatchPod(podName string, podNamespace string, c chan string) {
 
 	//opts := api.ListOptions{FieldSelector: fields.Set{"kind": "pod"}.AsSelector()}
 	opts := api.ListOptions{LabelSelector: labels.Set{"app": "nginx"}.AsSelector()}
@@ -47,3 +100,4 @@ func WatchPod(podName string, podNamespace string, c chan string) {
 		}
 	}
 }
+*/
